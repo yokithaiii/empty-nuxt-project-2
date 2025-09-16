a08b8ce0abbccdb257736c96f036e8fee2216dcc
spxrtxk <spartakvasilev1999@gmail.com>
2025-09-17 00:28:28 +0900

    fix: awdaw

--- app/plugins/error-handler.ts
+++ app/plugins/error-handler.ts
@@ -1,10 +1,10 @@
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.config.errorHandler = (error, instance, info) => {
-    alert(error)
+    //
   }
 
   // Also possible
   nuxtApp.hook('vue:error', (error, instance, info) => {
-    alert(error)
+    // 
   })
 })
