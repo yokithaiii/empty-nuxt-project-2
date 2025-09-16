8481266f003f55f5b293dfee7d586981c7266b08
spxrtxk <spartakvasilev1999@gmail.com>
2025-09-17 00:31:49 +0900

    fix: awdaw

--- app/error.vue
+++ app/error.vue
@@ -10,7 +10,7 @@ const handleError = () => clearError({ redirect: '/' })
 
 <template>
   <div>
-    <h2>{{ error?.statusCode }}</h2>
+    <h2>{{ error }}</h2>
     <button @click="handleError">Clear errors</button>
   </div>
 </template>
