e636ad7c80022fb8786cd5f5bcfabda8b59c9ecd
spxrtxk <spartakvasilev1999@gmail.com>
2025-09-16 22:47:35 +0900

    fix: awdaw

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import { useWebAppViewport } from 'vue-tg'
+
+onBeforeMount(() => {
+	useWebAppViewport().expand()
+})
+</script>
+
 <template>
 	<UApp>
 		<div class="bg-[var(--ui-bg)]">
