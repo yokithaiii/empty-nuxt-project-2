c4e935c4a1d7dd4ecec38d086987b6a40d6958cb
spxrtxk <spartakvasilev1999@gmail.com>
2025-10-24 04:22:40 +0300

    feat: Items page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -31,7 +31,7 @@ const colorMode = useColorMode();
 const cloudStorage = useWebAppCloudStorage();
 const route = useRoute();
 
-const ref = route.query.ref || '';
+const ref = route.query.start || '';
 
 const openModalEmail = () => {
 	store.value.phone = null;
